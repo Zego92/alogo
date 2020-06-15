ee3c2bf139cfe9cceae0c09ae191e0d019cbb5d5
Zego92 <Timchenco92@gmail.com>
2020-06-15 22:01:20 +0300

    Initial commit

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -54,7 +54,7 @@ class CategoryController extends Controller
         $category->car_id = $request->carId;
         $category->name = $request->name;
         $category->keywords = $request->keywords;
-        $category->slug = $slug . date('Y-m-d-h-m-s-v');
+        $category->slug = $slug . date('Y-m-d-h-m-s-v') . uniqid();
         $category->image = $imageName;
         $category->save();
         if ($category)
